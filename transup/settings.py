c6592b693a02dc1f748069f70d744abab068f32d
Otse Obande <otseobande@gmail.com>
2019-07-23 01:25:15 +0100

    call Csv function

--- transup/settings.py
+++ transup/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = env_config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env_config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = env_config('ALLOWED_HOSTS', cast=Csv)
+ALLOWED_HOSTS = env_config('ALLOWED_HOSTS', cast=Csv())
 
 # Application definition
 
